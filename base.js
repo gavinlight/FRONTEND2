8eb3cf24950c87d2c7800dbc2ad266b414025174
gavinlight <gavin.ligthart@gmail.com>
2013-09-06 18:46:44 +0200

    ADDED LOCAL SCOPE AND CLOSURE STUFF ALONG WITH OBJECT LITERAL

--- base.js
+++ base.js
@@ -1,11 +1,73 @@
+// CONSTRUCTOR OBJECT
 function Persoon(name) {
 	this.name = name;
 
 	this.speak = function() {
-		console.log('yo im' + this.name);
+		console.log('yo im ' + this.name);
 	}
 }
 
-var bob = persoon('bob');
+// PROTOTYPE
+Persoon.prototype.walk = function() {
+	console.log(this.name + ' is walking')
+}
 
-bob();
+Persoon.prototype.eat = function() {
+	console.log(this.name + ' is eating')
+}
+
+var bob = new Persoon('bob');
+var paul = new Persoon ('paul');
+
+bob.speak();
+bob.walk();
+paul.eat();
+
+// OBJECT LITERAL
+var persoonObject = {
+	name: 'Robert Rock',
+
+	speak: function () {
+		console.log('yo im ' + this.name);
+	},
+
+	walk: function () {
+		console.log(this.name + ' is walking')
+	},
+
+	eat: function () {
+		console.log(this.name + ' is eating')
+	}
+}
+
+persoonObject.speak();
+persoonObject.walk();
+persoonObject.eat();
+
+
+// LOCAL SCOPE
+function localScope(){
+	var iretator = 1;
+	var max = 10;
+	var min = 0;
+}
+
+var iretator = 1;
+var max = 10;
+var min = 0;
+
+// CLOSURE IS EIGENLIJK EEN FUNCTION IN EEN FUNCTION
+// DE INNERFUNCTION KAN BIJ DE VARIABELE EN REGELS CODE UIT DE OUTERFUNCTION
+// DE INNERFUNCTION KAN NOG CODE UITVOEREN NADAT DE OUTERFUNCTION AL ZIJN CODE HEEFT UITGEVOERD
+function outerFunction() {
+	var outerVar = 'string';
+
+	function innerFunction() {
+		var innerVar = outerVar;
+		console.log(outerVar);
+	}
+
+	innerFunction();
+}
+
+outerFunction();
